ffa32d4fd1c1306671f5b202c5cb4d29784a730d
annmayn <ntvirus333@gmail.com>
2022-02-15 20:30:13 +0545

    finish part two

--- advent_of_code/2020/day_15_rambunctious_recitation.py
+++ advent_of_code/2020/day_15_rambunctious_recitation.py
@@ -1,13 +1,14 @@
 def read_input(input_file: str) -> list[int]:
-    with open(input_file, 'r') as f:
+    with open(input_file, "r") as f:
         data = f.read()
-    return list(map(int, data.strip().split(',')))
+    return list(map(int, data.strip().split(",")))
 
-def part_one(data: list[int]):
+
+def main(data: list[int], n) -> int:
     val_to_ind_map: dict[int, int] = {k: i for i, k in enumerate(data)}
     curr_val = 0
     curr_ind = len(data)
-    while (curr_ind + 1) != 2020:
+    while (curr_ind + 1) != n:
         if curr_val in val_to_ind_map:
             prev_ind = val_to_ind_map[curr_val]
             val_to_ind_map[curr_val] = curr_ind
@@ -18,11 +19,13 @@ def part_one(data: list[int]):
         curr_ind += 1
     return curr_val
 
-    
 
 if __name__ == "__main__":
     input_file = "input/day_15.txt"
     data = read_input(input_file)
 
-    part_one_res = part_one(data)
+    part_one_res = main(data, 2020)
     print(f"{part_one_res=}")
+
+    part_two_res = main(data, 30000000)
+    print(f"{part_two_res=}")
