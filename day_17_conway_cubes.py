ab917eeaa43a65e426ce679d45e50e1476b3773c
annmayn <ntvirus333@gmail.com>
2022-02-27 12:08:21 +0545

    finished day 17 part 2

--- day_17_conway_cubes.py
+++ day_17_conway_cubes.py
@@ -1,8 +1,10 @@
-from typing import Iterator
+from typing import Coroutine, Iterator, Union
 
 
 NDimData = list[list[list[str]]]
 Coordinates = tuple[int, int, int]
+
+Coordinates4D = tuple[int, int, int, int]
 
 
 def read_input(input_file: str) -> NDimData:
@@ -12,29 +14,23 @@ def read_input(input_file: str) -> NDimData:
 
 
 def part_one(data: NDimData, num_runs: int = 6) -> int:
-    active_states, min_coordinates, max_coordinates = get_initial_state(data)
+    active_states = get_initial_state(data)
     for _ in range(num_runs):
-        active_states = run_cycle(active_states, min_coordinates, max_coordinates)
+        active_states = run_cycle(active_states)
     return len(active_states)
 
 
-def get_initial_state(data: NDimData) -> tuple[set[Coordinates], int, int]:
+def get_initial_state(data: NDimData) -> set[Coordinates]:
     active_states: set[Coordinates] = set()
-    min_coordinates = 0
-    max_coordinates = 0
     for i, grid in enumerate(data):
         for j, row in enumerate(grid):
             for k, col in enumerate(row):
                 if col == "#":
                     active_states.add((i, j, k))
-                    min_coordinates = min(min_coordinates, i)
-                    max_coordinates = max(max_coordinates, i)
-    return active_states, min_coordinates, max_coordinates
+    return active_states
 
 
-def run_cycle(
-    active_states: set[Coordinates], min_ord: int, max_ord: int
-) -> set[Coordinates]:
+def run_cycle(active_states: set[Coordinates]) -> set[Coordinates]:
     new_active_states: set[Coordinates] = set()
     for src in active_states:
         neighbors = get_neighbors(src)
@@ -64,9 +60,60 @@ def get_active_neighbors(node: Coordinates, active_states: set[Coordinates]) -> 
     return active_neighbors
 
 
+def part_two(data: NDimData, num_runs: int = 6) -> int:
+    active_states = get_initial_state_part_two(data)
+    for _ in range(num_runs):
+        active_states = run_cycle_part_two(active_states)
+    return len(active_states)
+
+
+def get_initial_state_part_two(data: NDimData) -> set[Coordinates4D]:
+    active_states: set[Coordinates4D] = set()
+    for x in range(1):
+        for i, grid in enumerate(data):
+            for j, row in enumerate(grid):
+                for k, col in enumerate(row):
+                    if col == "#":
+                        active_states.add((x, i, j, k))
+    return active_states
+
+
+def run_cycle_part_two(active_states: set[Coordinates4D]) -> set[Coordinates4D]:
+    new_active_states: set[Coordinates4D] = set()
+    for src in active_states:
+        neighbors = get_neighbors_part_two(src)
+        for node in neighbors:
+            active_neighbors = get_active_neighbors_part_two(node, active_states)
+            if node in active_states:
+                if active_neighbors in (2, 3):
+                    new_active_states.add(node)
+            else:  # inactive
+                if active_neighbors == 3:
+                    new_active_states.add(node)
+    return new_active_states
+
+
+def get_neighbors_part_two(node: Coordinates4D) -> Iterator[Coordinates4D]:
+    for i in range(-1, 2):
+        for j in range(-1, 2):
+            for k in range(-1, 2):
+                for w in range(-1, 2):
+                    yield (node[0] + i, node[1] + j, node[2] + k, node[3] + w)
+
+def get_active_neighbors_part_two(node: Coordinates4D, active_states: set[Coordinates4D]) -> int:
+    active_neighbors = 0
+    for node_neigh in get_neighbors_part_two(node):
+        if node_neigh != node and node_neigh in active_states:
+            active_neighbors += 1
+    return active_neighbors
+
+
 if __name__ == "__main__":
     input_file = "input/day_17.txt"
     data = read_input(input_file)
 
     part_one_res = part_one(data)
     print(f"{part_one_res=}")
+
+    part_two_res = part_two(data)
+    print(f"{part_two_res=}")
